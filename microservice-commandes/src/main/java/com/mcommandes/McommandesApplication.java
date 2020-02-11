efded01935ffb048c622fff494a5288ad2517b77
gael <gael.lehchibi@outlook.com>
2020-02-11 17:25:04 +0100

    Add eureka support

--- microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
+++ microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
@@ -2,8 +2,11 @@ package com.mcommandes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class McommandesApplication {
 
 	public static void main(String[] args) {
--- microservice-produits/src/main/java/com/mproduits/configuration/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configuration/ApplicationPropertiesConfiguration.java
@@ -1,10 +1,11 @@
 package com.mproduits.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("mes-configs")
 public class ApplicationPropertiesConfiguration {
     private int limitDeProduits;
